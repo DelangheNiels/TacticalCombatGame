2811aea37fb588a1f9452c9bf8b86c7b45dc9c37
DelangheNiels <delanghe.niels@telenet.be>
2023-09-25 11:22:10 +0200

    Initial commit

---
+++ TacticalCombatGame/Source/TacticalCombatGame/TacticalCombatGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TacticalCombatGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TACTICALCOMBATGAME_API ATacticalCombatGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
